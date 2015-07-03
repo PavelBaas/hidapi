6aee998ae08f4c543500aaf9bbccb19958d8d2c8
PavelBaas <PavelBaas@users.noreply.github.com>
2015-07-03 06:55:01 -0400

    Update hidtest.cpp

--- hidtest/hidtest.cpp
+++ hidtest/hidtest.cpp
@@ -19,176 +19,342 @@
 #include <string.h>
 #include <stdlib.h>
 #include "hidapi.h"
+#include <time.h>
 
 // Headers needed for sleeping.
 #ifdef _WIN32
-	#include <windows.h>
+#include <windows.h>
 #else
-	#include <unistd.h>
+#include <unistd.h>
 #endif
 
-int main(int argc, char* argv[])
+int sres, res, retry, silent = 0;
+char serialstring[256];
+unsigned char buf[256];
+char printbuf[30000];
+#define MAX_STR 255
+hid_device *handle;
+int i;
+int lastresult = 0;
+
+
+void
+bCmd (const char *str)
 {
-	int res;
-	unsigned char buf[256];
-	#define MAX_STR 255
-	wchar_t wstr[MAX_STR];
-	hid_device *handle;
-	int i;
+  unsigned short int val = 0;
+  sscanf (str, "%hd", &val);
+  memset (buf, 0, sizeof (buf));
+  buf[0] = 0;
+  buf[1] = val;
+  buf[2] = 0;
+  res = hid_write (handle, buf, 0x41);
+  if (res < 0)
+    {
+      printf ("Unable to write()\n");
+      printf ("Error: %ls\n", hid_error (handle));
+      exit (5);
+    }
+}
+
+
+void
+mCmd (char *str)
+{
+
+  lastresult = 0;
+
+  memset (buf, 0, sizeof (buf));
+  buf[0] = 0;
+  buf[1] = 0x60;
+  buf[2] = 0;
+
+  sprintf ((char *) &buf[3], "%s\r\n", str);
+  buf[2] = strlen ((char *) &buf[3]);
+
+  res = hid_write (handle, buf, 0x41);
+  if (res < 0)
+    {
+      printf ("Unable write()\n");
+      printf ("Error: %ls\n", hid_error (handle));
+    }
+}
+
+void
+gReply (int timeout = 3, int showresult = 1)
+{
+  timeout = timeout * 100;
+
+  int dpoint = 0;
+  int nodatamax = timeout;
+  memset (buf, 0, sizeof (buf));
+  int nodata = 0;
+  while (nodata < nodatamax)
+    {
+      res = 0;
+
+      while ((res == 0) && (nodata < nodatamax))
+	{
+	  memset (buf, 0, sizeof (buf));
+	  res = hid_read (handle, buf, sizeof (buf));
+	  if (res == 0)
+	    {
+	      nodata++;
+	      usleep (1 * 500);
+	    }
+	}
+      if (res < 0)
+	{
+	  printf ("Unable to read()\n");
+	  nodata++;
+	  exit (5);
+	}
+      if (res > 0)
+	{
+	  nodata = 0;
+
+	  if (res != 64)
+	    printf ("\nRead bytes %d:\n\n", res);
+
+	  if (buf[0] == 0x60)
+	    {
+	      int slen = buf[1];
+
+
+	      if ((dpoint + slen) > sizeof (printbuf))
+		{
+		  if (showresult)
+		    printf ("%s", printbuf);
+		  dpoint = 0;
+		}
+
+	      bcopy (&buf[2], &printbuf[dpoint], slen);
+	      dpoint = dpoint + slen;
+	      printbuf[dpoint] = 0x0;
+
+	      if (dpoint > 7)
+		{
+		  if (strncmp
+		      ("CMD OK\r\n", &printbuf[dpoint - 8],
+		       sizeof (printbuf) - dpoint) == 0)
+		    {
+		      lastresult = 1;
+
+		      break;
+		    }
+		  if (strncmp
+		      ("CMD Fail!\r\n", &printbuf[dpoint - 11],
+		       sizeof (printbuf) - dpoint) == 0)
+		    {
+		      lastresult = -1;
+		      break;
+		    }
+		}
+
+
+	    }
+	  else
+	    {
+	    }
+	}
+
+    }
+  if (showresult)
+    {
+      if ((lastresult == 1))
+	printf ("\n%s\n", printbuf);
+    }
+
+}
+
+
+void
+mCmdMacro (char *str, int showresult = 1)
+{
+
+  int failcounter = 0;
+  lastresult = -5;
+  while ((lastresult != 1) && (failcounter < 3))
+    {
+
+      if (failcounter > 0)
+	{
+	  printf ("Fail counter: %d,%s\n", failcounter, str);
+	  sleep (1);
+	  gReply (3, 0);
+	}
+
+      mCmd (str);
+      gReply (3, showresult);
+      failcounter++;
+    }
+
+  if (lastresult != 1)
+    {
+      printf ("Last result: %d\n", lastresult);
+      exit (5);
+    }
+}
+
+
+void
+cmode (int value)
+{
+  FILE *fp = fopen ("/sys/devices/platform/mt_usb/cmode", "w");
+  if (!fp)
+    return;
+  fprintf (fp, "%d\n", value);
+  fclose (fp);
+  if (value == 1)
+    {
+      sleep (3);
+    }
+}
+
+
+int
+main (int argc, char *argv[])
+{
 
 #ifdef WIN32
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv);
+  UNREFERENCED_PARAMETER (argc);
+  UNREFERENCED_PARAMETER (argv);
 #endif
 
-	struct hid_device_info *devs, *cur_dev;
-	
-	if (hid_init())
-		return -1;
+  if ((argc > 1) && (argv[1][0] == 'S'))
+    silent = 1;
 
-	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;	
-	while (cur_dev) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("  Release:      %hx\n", cur_dev->release_number);
-		printf("  Interface:    %d\n",  cur_dev->interface_number);
-		printf("\n");
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
+  struct tm t;
+  time_t t_of_day, bg_t_of_day;
+  int unknown1, unknown2, unknown3, unknown4, unknown5, unknown6, unknown7,
+    unknown8;
+  int mgdl, recordtype, recordnumber;
 
-	// Set up the command buffer.
-	memset(buf,0x00,sizeof(buf));
-	buf[0] = 0x01;
-	buf[1] = 0x81;
-	
 
-	// Open the device using the VID, PID,
-	// and optionally the Serial number.
-	////handle = hid_open(0x4d8, 0x3f, L"12345");
-	handle = hid_open(0x4d8, 0x3f, NULL);
-	if (!handle) {
-		printf("unable to open device\n");
- 		return 1;
-	}
+  cmode (1);
 
-	// Read the Manufacturer String
-	wstr[0] = 0x0000;
-	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read manufacturer string\n");
-	printf("Manufacturer String: %ls\n", wstr);
+  if (hid_init ())
+    return -1;
 
-	// Read the Product String
-	wstr[0] = 0x0000;
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read product string\n");
-	printf("Product String: %ls\n", wstr);
 
-	// Read the Serial Number String
-	wstr[0] = 0x0000;
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read serial number string\n");
-	printf("Serial Number String: (%d) %ls", wstr[0], wstr);
-	printf("\n");
 
-	// Read Indexed String 1
-	wstr[0] = 0x0000;
-	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	if (res < 0)
-		printf("Unable to read indexed string 1\n");
-	printf("Indexed String 1: %ls\n", wstr);
+  // Open the device using the VID, PID,
+  // and optionally the Serial number.
 
-	// Set the hid_read() function to be non-blocking.
-	hid_set_nonblocking(handle, 1);
-	
-	// Try to read from the device. There shoud be no
-	// data here, but execution should not block.
-	res = hid_read(handle, buf, 17);
+  retry = 12;
 
-	// Send a Feature Report to the device
-	buf[0] = 0x2;
-	buf[1] = 0xa0;
-	buf[2] = 0x0a;
-	buf[3] = 0x00;
-	buf[4] = 0x00;
-	res = hid_send_feature_report(handle, buf, 17);
-	if (res < 0) {
-		printf("Unable to send a feature report.\n");
+  while ((!handle) && (retry > 0))
+    {
+
+      handle = hid_open (0x1a61, 0x3460, NULL);
+
+      if (!handle)
+	{
+	  if (!silent)
+	    printf ("Sleeping\n");
+	  sleep (1);
+	  retry = retry - 1;
 	}
 
-	memset(buf,0,sizeof(buf));
+    }
 
-	// Read a Feature Report from the device
-	buf[0] = 0x2;
-	res = hid_get_feature_report(handle, buf, sizeof(buf));
-	if (res < 0) {
-		printf("Unable to get a feature report.\n");
-		printf("%ls", hid_error(handle));
-	}
-	else {
-		// Print out the returned buffer.
-		printf("Feature Report\n   ");
-		for (i = 0; i < res; i++)
-			printf("%02hhx ", buf[i]);
-		printf("\n");
-	}
+  if (!handle)
+    {
+      printf ("Unable to open\n");
+      return 1;
+    }
 
-	memset(buf,0,sizeof(buf));
+  hid_set_nonblocking (handle, 1);
+  res = hid_read (handle, buf, 64);
 
-	// Toggle LED (cmd 0x80). The first byte is the report number (0x1).
-	buf[0] = 0x1;
-	buf[1] = 0x80;
-	res = hid_write(handle, buf, 17);
-	if (res < 0) {
-		printf("Unable to write()\n");
-		printf("Error: %ls\n", hid_error(handle));
-	}
-	
 
-	// Request state (cmd 0x81). The first byte is the report number (0x1).
-	buf[0] = 0x1;
-	buf[1] = 0x81;
-	hid_write(handle, buf, 17);
-	if (res < 0)
-		printf("Unable to write() (2)\n");
+  // Set the hid_read() function to be non-blocking.
 
-	// Read requested state. hid_read() has been set to be
-	// non-blocking by the call to hid_set_nonblocking() above.
-	// This loop demonstrates the non-blocking nature of hid_read().
-	res = 0;
-	while (res == 0) {
-		res = hid_read(handle, buf, sizeof(buf));
-		if (res == 0)
-			printf("waiting...\n");
-		if (res < 0)
-			printf("Unable to read()\n");
-		#ifdef WIN32
-		Sleep(500);
-		#else
-		usleep(500*1000);
-		#endif
-	}
+  gReply (1, 0);
 
-	printf("Data read:\n   ");
-	// Print out the returned buffer.
-	for (i = 0; i < res; i++)
-		printf("%02hhx ", buf[i]);
-	printf("\n");
+  bCmd ("4");
+  gReply (3, 0);
+  bCmd ("5");
+  gReply (3, 0);
+  bCmd ("21");
+  gReply (3, 0);
+  bCmd ("1");
+  gReply (3, 0);
 
-	hid_close(handle);
+  mCmdMacro ("$serlnum?", 0);
 
-	/* Free static HIDAPI objects. */
-	hid_exit();
+  sres = sscanf (printbuf, "%s", serialstring);
+
+  if (sres != 1)
+    {
+      exit (5);
+    }
+
+  mCmdMacro ("$date?", 0);
+
+  sres = sscanf (printbuf, "%d,%d,%d", &t.tm_mon, &t.tm_mday, &t.tm_year);
+
+  if (sres != 3)
+    {
+      exit (5);
+    }
+
+  t.tm_year = t.tm_year + 2000;
+
+  mCmdMacro ("$time?", 0);
+  sres = sscanf (printbuf, "%d,%d", &t.tm_hour, &t.tm_min);
+
+  if (sres != 2)
+    {
+      exit (5);
+    }
+
+  t.tm_sec = 0;
+  t.tm_isdst = -1;
+  t.tm_year = t.tm_year - 1900;
+  t.tm_mon = t.tm_mon - 1;
+
+  t_of_day = mktime (&t);
+
+  mCmdMacro ("$result?", 0);
+  sres =
+    sscanf (printbuf, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",
+	    &recordtype, &recordnumber, &t.tm_mon, &t.tm_mday, &t.tm_year,
+	    &t.tm_hour, &t.tm_min, &unknown1, &unknown2, &unknown3, &unknown4,
+	    &unknown5, &unknown6, &mgdl, &unknown7, &unknown8);
+
+  if (sres != 16)
+    {
+      exit (5);
+    }
+
+  if (recordtype != 0)
+    {
+      exit (5);
+    }
+
+
+  t.tm_sec = 0;
+  t.tm_isdst = -1;
+  t.tm_year = t.tm_year + 2000;
+  t.tm_year = t.tm_year - 1900;
+  t.tm_mon = t.tm_mon - 1;
+  bg_t_of_day = mktime (&t);
+  long int timediff = t_of_day - bg_t_of_day;
+
+  float mmol = mgdl / 18.018;
+  printf
+    ("Serial: %s :: Time since last bg: %d secs :: %.1f mmol/l / %d mg/dl\n",
+     serialstring, timediff, mmol, mgdl);
+
+  hid_close (handle);
+  /* Free static HIDAPI objects. */
+  hid_exit ();
 
 #ifdef WIN32
-	system("pause");
+  system ("pause");
 #endif
 
-	return 0;
+  cmode (0);
+
+  return 0;
 }
